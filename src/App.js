d7523c9e45f58ce27e1ae142311f01ca66968fc3
Angel Cornejo <self.angelcornejo@gmail.com>
2022-01-25 17:36:13 -0700

    Adds basic splash concept and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from "react";
+import './styles/App.scss';
 
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+const App = () => {
+  let page = '';
+
+  page = (
+    <main>
+      <h2 className="hi">Hi.</h2>
+      <h2 className="my-name">My name is <br className="name-break"/>Angel Cornejo and I'm a software developer.</h2>
+    </main>
+  )
+
+  return page;
+
 }
 
 export default App;
