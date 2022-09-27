d37aedea442111fef920ada63db66d326d81f51c
Angel Cornejo <self.angelcornejo@gmail.com>
2022-09-27 12:56:37 -0700

    Adds contact tab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import './styles/App.scss';
-import battleship from './images/battleship.jpg';
-import blog from './images/blog.jpg';
-import kanban from './images/kanban.jpg';
 import Navbar from './components/Navbar';
-import ProjectCard from './components/ProjectCard';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 const App = () => {
   const [currentTab, setCurrentTab] = useState('portfolio');
@@ -14,59 +12,12 @@ const App = () => {
   };
   let page = '';
 
-  const battleShipDescription =
-    'An Implementation of the classic game Battleship.';
-
-  const battleshipTech = ['React,', 'CSS,', 'Jest,', 'react-dnd'];
-
-  const kanbanDescription = 'A Trello inspired kanban board.';
-
-  const kanbanTech = ['React,', 'Sass,', 'react-beautiful-dnd'];
-
-  const blogDescription = 'A social media blog hybrid site';
-  const blogTech = [
-    'MongoDB,',
-    'Express,',
-    'React,',
-    'Node',
-    'Sass,',
-    'Passportjs',
-  ];
-
   page = (
     <div className="app">
       <Navbar currentTab={currentTab} changeTab={changeTab} />
       <main>
-        <div className="about">
-          <h3>Software Developer</h3>
-          <p>Always striving for growth</p>
-        </div>
-        <div className="projects">
-          <ProjectCard
-            name="battleship"
-            description={battleShipDescription}
-            tech={battleshipTech}
-            src={battleship}
-            source="https://github.com/cornejoangel/battleship"
-            live="https://cornejoangel.github.io/battleship/"
-          />
-          <ProjectCard
-            name="social blog"
-            description={blogDescription}
-            tech={blogTech}
-            src={blog}
-            source="https://github.com/cornejoangel/blog-api"
-            live="https://cryptic-cove-57692.herokuapp.com/"
-          />
-          <ProjectCard
-            name="kanban board"
-            description={kanbanDescription}
-            tech={kanbanTech}
-            src={kanban}
-            source="https://github.com/cornejoangel/kanban-board"
-            live="https://cornejoangel.github.io/kanban-board/"
-          />
-        </div>
+        {currentTab === 'portfolio' && <Portfolio />}
+        {currentTab === 'contact' && <Contact />}
       </main>
     </div>
   );
---
+++ src/components/Portfolio.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import battleship from '../images/battleship.jpg';
+import blog from '../images/blog.jpg';
+import kanban from '../images/kanban.jpg';
+import ProjectCard from './ProjectCard';
+
+const Portfolio = () => {
+  let portfolio = '';
+  const battleShipDescription =
+    'An Implementation of the classic game Battleship.';
+
+  const battleshipTech = ['React,', 'CSS,', 'Jest,', 'react-dnd'];
+
+  const kanbanDescription = 'A Trello inspired kanban board.';
+
+  const kanbanTech = ['React,', 'Sass,', 'react-beautiful-dnd'];
+
+  const blogDescription = 'A social media blog hybrid site';
+  const blogTech = [
+    'MongoDB,',
+    'Express,',
+    'React,',
+    'Node',
+    'Sass,',
+    'Passportjs',
+  ];
+  portfolio = (
+    <div className="portfolio-content">
+      <div className="about">
+        <h3>Software Developer</h3>
+        <p>Always striving for growth</p>
+      </div>
+      <div className="projects">
+        <ProjectCard
+          name="battleship"
+          description={battleShipDescription}
+          tech={battleshipTech}
+          src={battleship}
+          source="https://github.com/cornejoangel/battleship"
+          live="https://cornejoangel.github.io/battleship/"
+        />
+        <ProjectCard
+          name="social blog"
+          description={blogDescription}
+          tech={blogTech}
+          src={blog}
+          source="https://github.com/cornejoangel/blog-api"
+          live="https://cryptic-cove-57692.herokuapp.com/"
+        />
+        <ProjectCard
+          name="kanban board"
+          description={kanbanDescription}
+          tech={kanbanTech}
+          src={kanban}
+          source="https://github.com/cornejoangel/kanban-board"
+          live="https://cornejoangel.github.io/kanban-board/"
+        />
+      </div>
+    </div>
+  );
+
+  return portfolio;
+};
+
+export default Portfolio;
